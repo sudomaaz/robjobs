5ded572371803c00896e0eaaa81c7caa36f94a68
mzz14 <maaz1407@gmail.com>
2021-06-25 22:28:23 +0530

    crud job operations

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -1,18 +1,60 @@
 import jobModel from "../models/jobs.js";
 
-// All Jobs request logic => GET /api/v1/jobs
+// fetch all jobs => GET /api/v1/jobs
 export const allJobs = async (req, res, next) => {
+  const jobs = await jobModel.find();
   res.status(200).send({
     success: true,
     message: "This route displays all jobs",
+    data: jobs,
   });
 };
 
+// Create a new Job => POST /api/v1/jobs
 export const newJob = async (req, res, next) => {
   const job = await jobModel.create(req.body);
   res.status(201).json({
     success: true,
     message: "Job has been created successfully",
     data: job,
+  });
+};
+
+// Get a specific job by its id => GET /api/v1/job/:id
+export const fetchJob = async (req, res, next) => {
+  const job = await jobModel.findById(req.params.id);
+  if (!job) return;
+  res.status(201).json({
+    success: true,
+    message: "Job is found",
+    data: job,
+  });
+};
+
+// update a specific job by its id => PUT /api/v1/job/:id
+export const updateJob = async (req, res, next) => {
+  let job = await jobModel.findById(req.params.id);
+  if (!job) return;
+  job = await jobModel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(201).json({
+    success: true,
+    message: "This Job has been updated successfully",
+    data: job,
+  });
+};
+
+// delete a specific job by its id => DELETE /api/v1/job/:id
+export const deleteJob = async (req, res, next) => {
+  let job = await jobModel.findById(req.params.id);
+  if (!job) return;
+  await job.remove();
+  res.status(200).send({
+    success: true,
+    message: "This job has been deleted successfully",
+    data: null,
   });
 };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { allJobs, newJob } from "../controllers/jobs.js";
+import {
+  allJobs,
+  newJob,
+  updateJob,
+  deleteJob,
+  fetchJob,
+} from "../controllers/jobs.js";
 
 // instantiate express router
 const router = express.Router();
 
-// Route to get all/create new jobs
+// Route to get all available jobs
 router.route("/jobs").get(allJobs).post(newJob);
+
+//Route to read/update/delete a specific job by its id
+router.route("/job/:id").get(fetchJob).put(updateJob).delete(deleteJob);
 
 export default router;
