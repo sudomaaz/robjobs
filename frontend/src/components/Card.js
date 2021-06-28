d8e86471354ee9bbd9cecd1f83a1cf692ee4eb6f
sudomaaz <maazm0649@gmail.com>
2021-06-28 22:59:29 +0530

    building frontend

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
   return (
     <div className="col" key={props.info._id}>
-      <div className="card bg-danger mb-3">
+      <div className="card bg-danger mb-3" key={props.info._id}>
         <div className="card-header">{props.info.title}</div>
         <div className="card-body">
           <h4 className="card-title">{props.info.company}</h4>
